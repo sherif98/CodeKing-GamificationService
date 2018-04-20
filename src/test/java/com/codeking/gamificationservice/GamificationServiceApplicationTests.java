64ec4e6c135fdd573911c5872d2a55af2d99b760
Sherif Hamdy <sherif.hamdy.1995@gmail.com>
2018-04-20 15:21:15 +0200

    Initial commit

---
+++ src/test/java/com/codeking/gamificationservice/GamificationServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeking.gamificationservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamificationServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
