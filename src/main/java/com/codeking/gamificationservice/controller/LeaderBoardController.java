6d01e1b46a34057732ea52e3300ae97930f0ca41
Sherif Hamdy <sherif.hamdy.1995@gmail.com>
2018-06-24 22:11:11 +0200

    added controller for leaderboard

---
+++ src/main/java/com/codeking/gamificationservice/controller/LeaderBoardController.java
@@ -0,0 +1,24 @@
+package com.codeking.gamificationservice.controller;
+
+import com.codeking.gamificationservice.domain.User;
+import com.codeking.gamificationservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/leaderboard")
+public class LeaderBoardController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @GetMapping
+    public List<User> getLeaderBoard() {
+        return userRepository.findTop10UsersByOrderByScoreDesc();
+    }
+}
