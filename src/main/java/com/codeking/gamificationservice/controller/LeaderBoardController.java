b04b1c0e0177b29b39bb10e72c706845f7b6820f
sherif <sherif.hamdy.1995@gmail.com>
2018-07-27 23:25:24 +0200

    moved configurations to config server

--- src/main/java/com/codeking/gamificationservice/controller/LeaderBoardController.java
+++ src/main/java/com/codeking/gamificationservice/controller/LeaderBoardController.java
@@ -3,6 +3,7 @@ package com.codeking.gamificationservice.controller;
 import com.codeking.gamificationservice.domain.User;
 import com.codeking.gamificationservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,12 @@ import java.util.List;
 @RequestMapping("/leaderboard")
 public class LeaderBoardController {
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
 
-    @GetMapping
-    public List<User> getLeaderBoard() {
-        return userRepository.findTop10UsersByOrderByScoreDesc();
-    }
+  @GetMapping
+  public List<User> getLeaderBoard() {
+    return userRepository.findTop10UsersByOrderByScoreDesc();
+  }
 }
