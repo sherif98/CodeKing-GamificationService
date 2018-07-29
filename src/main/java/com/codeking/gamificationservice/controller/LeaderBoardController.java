ced96e7ea73d20bf33d92d0309bbaf34e4c9cc66
sherif <sherif.hamdy.1995@gmail.com>
2018-07-30 01:01:03 +0200

    added spring security

--- src/main/java/com/codeking/gamificationservice/controller/LeaderBoardController.java
+++ src/main/java/com/codeking/gamificationservice/controller/LeaderBoardController.java
@@ -4,6 +4,7 @@ import com.codeking.gamificationservice.domain.User;
 import com.codeking.gamificationservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,11 @@ public class LeaderBoardController {
   @GetMapping
   public List<User> getLeaderBoard() {
     return userRepository.findTop10UsersByOrderByScoreDesc();
+  }
+
+
+  @DeleteMapping
+  public void deleteLeaderBoard() {
+    System.out.println("deleting leader board");
   }
 }
